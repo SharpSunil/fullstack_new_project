07bf5533af37b04a9ab5629994788765587b16af
Sunil Shelke <shelkesunil072@gmail.com>
2026-02-04 18:20:44 +0530

    new

--- frontend/src/pages/FormPage.jsx
+++ frontend/src/pages/FormPage.jsx
@@ -8,13 +8,13 @@ const FormPage = () => {
                 <div className="form-cont cont">
                     <h2>Form Heading</h2>
                     <div className="form-group">
-                        <label>Name:</label>
+
                         <input type="text" placeholder="Enter your name" />
-                        <label>Email:</label>
+
                         <input type="email" placeholder="Enter your email" />
-                        <label>Upload Image</label>
-                        <input type="file" />
-                        <button type="submit">Submit</button>
+
+                        <input className='files' type="file" />
+                        <button className='btn' type="submit">Submit</button>
                     </div>
                 </div>
             </div>
